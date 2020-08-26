d28d5e65d64557670aff42de273dd22ec8fe6804
Ahmed Elhegery <ahmed.3laa.2020@gmail.com>
2020-08-26 12:58:59 +0200

    1-vue startup

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data (){
+  return{
+    api_key:'641d47bc20e1dabe6a69d811be1a11af'
+  }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
